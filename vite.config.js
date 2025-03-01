ca955a26a5b687a59a98037473fce830f819484e
Tania Salvatella <tsalvatellap@gmail.com>
2025-03-01 10:08:55 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/promo_b_modulo_3_ListadoDePaises/",
   server:{
     watch:{
       usePolling: true,
